808e541735b9aac91fb986bd91b56e2b81ec7d29
ChaseBENNC9 <104808214+ChaseBENNC9@users.noreply.github.com>
2024-06-24 09:51:34 +1200

    refactor; seperated into more methods

--- gps_code/gps_code.ino
+++ gps_code/gps_code.ino
@@ -1,43 +1,52 @@
 #include <TinyGPSPlus.h>
 #include <SoftwareSerial.h>
 
-static const int RXPin = 0, TXPin = 1;
-static const uint32_t GPSBaud = 9600;
+static const uint32_t BaudRate = 9600; //Baud Rate for the Serial Connections
 
-static const int BT_RX = 12, BT_TX = 11;
+static const int GPS_RX = 0, GPS_TX = 1; //Digital Pins for the Gps module
 
-// The TinyGPSPlus object
+static const int BT_RX = 12, BT_TX = 11; //Digital pins for the BT Module
+
+// Setting up the TinyGPSPlus object
 TinyGPSPlus gps;
 
-// The serial connection to the GPS device
-SoftwareSerial gpsSerial(RXPin, TXPin);
-// The serial connection to the Bluetooth Module
-SoftwareSerial BTSerial(BT_RX,BT_TX);
+// Initalise the serial connection to the GPS device
+SoftwareSerial gpsSerial(GPS_RX, GPS_TX);
+// Initalise the serial connection to the Bluetooth Module
+SoftwareSerial BTSerial(BT_RX, BT_TX);
 
 void setup() {
-  Serial.begin(GPSBaud);
-  BTSerial.begin(GPSBaud);
-  gpsSerial.begin(GPSBaud);
+  Serial.begin(BaudRate);
+  BTSerial.begin(BaudRate);
+  gpsSerial.begin(BaudRate);
+
+  BTSerial.println("Beginning Connection");
 }
 
-void loop() { //As long as the GPS is available
-  if (gpsSerial.available() > 0)
+void loop() {  //As long as the GPS is available
+  GetGPSData();
+  CheckConnectionTimeout();
+}
+void GetGPSData() {
+  if (gpsSerial.available() > 0)  //If there is at least 1 gps connection
     if (gps.encode(gpsSerial.read())) {
-      if(CheckValidLocation())
+      if (CheckValidLocation())
         DisplayLocation();
       else
-        Serial.println("ERROR: Something went wrong while Retrieving Location Data");
+        BTSerial.println("ERROR: The Location Data was retrieved but it was INVALID");
     }
-  if (millis() > 5000 && gps.charsProcessed() < 10) {
-    Serial.println(F("No GPS detected: check wiring."));
-  }
 }
 
-bool CheckValidLocation() {
+void CheckConnectionTimeout() { //After 5 seconds this will print a message informing the user that there was a timeout
+  if (millis() > 5000 && gps.charsProcessed() < 10) {
+    BTSerial.println(F("TIMEOUT: GPS has not been synced with the satellite"));
+  }
+}
+bool CheckValidLocation() { //Returns whether the encoded gps data was valid or not
   return (gps.location.isValid());
 }
 
-void DisplayLocation() {
+void DisplayLocation() { //Prints out Lat and Long to the Bluetooth Serial Connection 
 
   BTSerial.print(gps.location.lat(), 6);
   BTSerial.print(F(","));
